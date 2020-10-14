66b578cc19e1430b81c7386b88314e9286f85d50
Jesus Medina <medinabotamail@gmail.com>
2020-10-14 17:50:03 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Instapago</title>
+</head>
+<body>
+    <h1 class="header">Pasarela de pago</h1>
+</body>
+</html>
